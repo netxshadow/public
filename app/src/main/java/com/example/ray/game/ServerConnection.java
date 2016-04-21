1ed8e4da0dcc37f1266781ddf64efef0d87c3644
netxshadow <ray@jump.ua>
2016-04-21 22:44:08 +0300

    last

--- app/src/main/java/com/example/ray/game/ServerConnection.java
+++ app/src/main/java/com/example/ray/game/ServerConnection.java
@@ -1,6 +1,10 @@
 package com.example.ray.game;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,13 +13,42 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Handler;
 
 public class ServerConnection {
+
+    String response;
+
     public static final String GAME_SERVER = "http://game.nmi.com.ua/gameplay.php";
+
+    public String request2(String params){
+        Timer t = new Timer();
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                response = sendRequestToServer("actionID=gameConnect");
+            }
+        }, 0, 1000);
+        return response;
+    }
+
+    public String request1(String params){
+        String response = "";
+        ServerTask st = new ServerTask();
+        st.execute(params);
+        return response;
+    }
+
     public String sendRequestToServer(String params){
+
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
         String result = "";
+
         try {
             URL url = new URL(GAME_SERVER);
             urlConnection = (HttpURLConnection) url.openConnection();
@@ -24,9 +57,11 @@ public class ServerConnection {
             urlConnection.setRequestMethod("POST");
             urlConnection.setDoInput(true);
             urlConnection.setDoOutput(true);
+            urlConnection.connect();
             OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream());
             writer.write(params);
             writer.flush();
+
             InputStream inputStream = urlConnection.getInputStream();
             StringBuffer buffer = new StringBuffer();
             reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -37,6 +72,8 @@ public class ServerConnection {
             result = buffer.toString();
             writer.close();
             reader.close();
+            urlConnection.disconnect();
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (ProtocolException e) {
@@ -44,6 +81,27 @@ public class ServerConnection {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Log.d("GAME_LOGS", "response from server: " + result);
+
         return result;
     }
+
+    class ServerTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            for(int i = 1; i<=10; i++) {
+                sendRequestToServer(params[0]);
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+    }
+
 }

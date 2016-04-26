7de306a201cb0cb9cbaa0d368e98f2f4fe44106d
netxshadow <ray@jump.ua>
2016-04-26 19:40:48 +0300

    last

--- app/src/main/java/com/example/ray/game/GameActivity.java
+++ app/src/main/java/com/example/ray/game/GameActivity.java
@@ -25,10 +25,18 @@ public class GameActivity extends BaseActivity {
     final String GAME_LOGS = "GAME_LOGS";
     ServerConnection sc = new ServerConnection();
     String response;
+    String result;
     String gameID;
-    Boolean gamePlay;
-    String s0;
-    Handler h;
+    boolean gameConnect;
+    boolean gamePlay;
+    boolean gameCheck;
+
+    String playerID;
+    String s0, p1, p2;
+    Handler handler;
+    boolean isCanMove;
+    boolean setMove = false;
+    String movePoint;
     //private Button[][] buttons  =   new Button[2][5];
     private TableLayout tablelayout;
     TextView tvSystem, tvP1, tvP2;
@@ -46,54 +54,116 @@ public class GameActivity extends BaseActivity {
 
         buildGameField();
 
-        gameConnect();
-
         gamePlay = true;
+        gameConnect = true;
+        gameCheck = true;
 
         startGameLoop();
 
     }
 
-    private void gameConnect(){
-
-        h  = new Handler(){
-            public void handleMessage(android.os.Message msg){
-                gameID = msg.obj.toString();
-            }
-        };
-
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                response = sc.sendRequestToServer("actionID=gameConnect");
-                gameID = getJsonValue(response, "gameID");
-                Message msg = h.obtainMessage(0, gameID);
-                h.sendMessage(msg);
-            }
-        });
-        t.start();
-
-    }
-
     private void startGameLoop(){
 
-        h  = new Handler(){
-            public void handleMessage(android.os.Message msg){
-                tvSystem.setText(msg.obj.toString());
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message message){
+                Bundle bandle = message.getData();
+                String actionID = bandle.getString("actionID");
+                switch(actionID){
+                    case "gameConnect":
+                        if(bandle.getString("error").equals("ok")){
+                            gameID = bandle.getString("gameID");
+                            playerID = bandle.getString("playerID");
+                            Toast.makeText(GameActivity.this, "Connected! GameID: " + gameID + " PlayerID: " + playerID, Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(GameActivity.this, bandle.getString("error"), Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case "gameCheck":
+                        if(bandle.getString("error").equals("ok")){
+                            s0 = bandle.getString("s0");
+                            p1 = bandle.getString("p1");
+                            p2 = bandle.getString("p2");
+                            if(bandle.getString("move").equals("1")){
+                                isCanMove = true;
+                            }else{
+                                isCanMove = false;
+                            }
+                        }else{
+                            //Toast.makeText(GameActivity.this, bandle.getString("error"), Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case "gameMove":
+
+                        break;
+                    default:
+
+                        break;
+                }
             }
         };
 
-        Thread t = new Thread(new Runnable() {
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
+                Bundle bandle = new Bundle();
                 while(gamePlay) {
-                    response = sc.sendRequestToServer("actionID=checkGamePlay&gameID=" + gameID);
-                    if(response.equals("wait")){
-                        Log.d(GAME_LOGS, "wait for gamers connect...");
-                    }else{
-                        s0 = getJsonValue(response, "s0");
-                        Message msg = h.obtainMessage(0, s0);
-                        h.sendMessage(msg);
+
+                    if(gameConnect){
+                        try {
+                            response = sc.sendRequestToServer("actionID=gameConnect");
+                            bandle.putString("actionID", "gameConnect");
+                            if(getJsonValue(response, "error").equals("ok")){
+                                bandle.putString("gameID", getJsonValue(response, "gameID"));
+                                bandle.putString("playerID", getJsonValue(response, "playerID"));
+                                gameConnect = false;
+                            }
+                            bandle.putString("error", getJsonValue(response, "error"));
+                            Message message1 = handler.obtainMessage();
+                            message1.setData(bandle);
+                            handler.sendMessage(message1);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Log.d(GAME_LOGS, "err", e);
+                        }
+                    }
+
+                    if(setMove){
+                        try {
+                            response = sc.sendRequestToServer("actionID=gameMove&gameID=" + gameID + "&playerID=" + playerID + "&movePoint=" + movePoint);
+                            bandle.putString("actionID", "gameMove");
+                            if(getJsonValue(response, "error").equals("ok")){
+                                isCanMove = false;
+                                setMove = false;
+                            }
+                            bandle.putString("error", getJsonValue(response, "error"));
+                            Message message2 = handler.obtainMessage();
+                            message2.setData(bandle);
+                            handler.sendMessage(message2);
+                        } catch (Exception e) {
+                            Log.d(GAME_LOGS, "err", e);
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if(gameCheck){
+                        try {
+                            response = sc.sendRequestToServer("actionID=gameCheck&gameID=" + gameID + "&playerID=" + playerID);
+                            bandle.putString("actionID", "gameCheck");
+                            if (getJsonValue(response, "error").equals("ok")) {
+                                bandle.putString("s0", getJsonValue(response, "s0"));
+                                bandle.putString("p1", getJsonValue(response, "p1"));
+                                bandle.putString("p2", getJsonValue(response, "p2"));
+                                bandle.putString("move", getJsonValue(response, "move"));
+                            }
+                            bandle.putString("error", getJsonValue(response, "error"));
+                            Message message3 = handler.obtainMessage();
+                            message3.setData(bandle);
+                            handler.sendMessage(message3);
+                        } catch (Exception e) {
+                            Log.d(GAME_LOGS, "err", e);
+                            e.printStackTrace();
+                        }
                     }
 
                     try {
@@ -104,8 +174,7 @@ public class GameActivity extends BaseActivity {
                 }
             }
         });
-        t.start();
-
+        thread.start();
     }
 
     private void stopGameLoop(){
@@ -173,10 +242,15 @@ public class GameActivity extends BaseActivity {
         {
             Button button = (Button) view;
 
-            Log.d(GAME_LOGS, "Cell: x=" + x + " y=" + y + " Value: " + value);
-            tvP1.setText(value + "");
-            button.setVisibility(View.INVISIBLE);
-            stopGameLoop();
+            if(isCanMove) {
+                setMove = true;
+                movePoint = value+"";
+                Log.d(GAME_LOGS, "Cell: x=" + x + " y=" + y + " Value: " + value);
+                tvP1.setText(value + "");
+                button.setVisibility(View.INVISIBLE);
+            }else{
+                Toast.makeText(GameActivity.this, "Wait a second...", Toast.LENGTH_SHORT).show();
+            }
 
         }
     }
